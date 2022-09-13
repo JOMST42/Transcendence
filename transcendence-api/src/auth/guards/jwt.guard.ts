b8fec0d10fd911e7c6512b1090c2336778c4947a
mleblanc <mleblanc@student.42quebec.com>
2022-09-13 15:24:44 -0400

    add jwt constructor

--- transcendence-api/src/auth/guards/jwt.guard.ts
+++ transcendence-api/src/auth/guards/jwt.guard.ts
@@ -1,3 +1,7 @@
 import { AuthGuard } from '@nestjs/passport';
 
-export class JwtGuard extends AuthGuard('jwt') {}
+export class JwtGuard extends AuthGuard('jwt') {
+  constructor() {
+    super();
+  }
+}
